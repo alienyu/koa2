55c281313dd960f7a950711943f15127aa8d6a1d
Alien Yu <alienyu@AliendeMacBook-Pro.local>
2018-05-30 11:48:42 +0800

    native router

--- middleware/logger-async.js
+++ middleware/logger-async.js
@@ -1,13 +1,13 @@
-function log( ctx ) {
-    console.log( ctx.method, ctx.header.host + ctx.url )
+function log(ctx) {
+    console.log(ctx.method, ctx.header.host + ctx.url)
 }
 
 module.exports = function () {
-  return async function ( ctx, next ) {
-    log(ctx);
-    console.log("start3")
-    await next()
-    console.log("start4")
+    return async function (ctx, next) {
+        log(ctx);
+        console.log("start3")
+        await next()
+        console.log("start4")
 
-  }
+    }
 }
